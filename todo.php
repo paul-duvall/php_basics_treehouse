4382cfc39643fcb3adea604850f20f57c4e2bc22
Paul Duvall <duvallpj@gmail.com>
2018-11-05 18:31:21 +0000

    Update to todo.php, adding functionality to print complete tasks, incomplete tasks or all tasks

--- todo.php
+++ todo.php
@@ -3,11 +3,20 @@
 // The array is included from a separate document
 include 'list.php';
 
+// $status should be set to true (to display complete tasks), false (to display incomplete tasks) or 'all' to display all
+$status = false;
+$filter = array();
 
 // A foreach loop can access both the key and the value as follows:
 foreach($list as $key => $item){
-  echo $key . " = " . $item['title'] . "<br />\n";
+  if($status === 'all' || $item['complete'] == $status) {
+    $filter[] = $key;
+  }
 }
+
+//echo '<pre>';
+//var_dump($filter, $list);
+//echo '</pre>';
 
 // Here, a foreach loop is used in conjunction with a table to produce the information in the array on a page
 
@@ -17,13 +26,13 @@ echo '<th>Title</th>';
 echo '<th>Priority</th>';
 echo '<th>Due Date</th>';
 echo '<th>Complete</th>';
-foreach($list as $item){
+foreach($filter as $id){
   echo '<tr>';
-  echo '<td>' . $item['title'] . "</td>\n";
-  echo '<td>' . $item['priority'] . "</td>\n";
-  echo '<td>' . $item['due'] . "</td>\n";
+  echo '<td>' . $list[$id]['title'] . "</td>\n";
+  echo '<td>' . $list[$id]['priority'] . "</td>\n";
+  echo '<td>' . $list[$id]['due'] . "</td>\n";
   echo '<td>';
-  if($item['complete']){
+  if($list[$id]['complete']){
     echo 'Yes';
   } else {
     echo 'No';
@@ -33,12 +42,12 @@ foreach($list as $item){
 }
 echo '</table>';
 
-// Once these arrays are created separately, they can be added to a multi-dimensional array:
+// If arrays are created separately, they can be added to a multi-dimensional array:
 
 //$list = array($task1, $task2);
 // This line is not neccessary if the individual arrays are declared like this:
 // $list[] = array(items in here);
-// It is then automatically added to the $list array
+// Each array is then automatically added to the $list array
 // Keys can then be specified for each item in the outer array:
 // $list[Task 1];
 
